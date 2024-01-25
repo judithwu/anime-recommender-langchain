c041933186487e78c7f831b6e8a47104cbce0a7b
judithwu <judithwu@umich.edu>
2024-01-25 01:44:57 -0500

    update run.py

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from anime import generate_response
 # Page title
 st.set_page_config(page_title='🦜🔗 Anime Recommendation System')
 st.title('🦜🔗 Anime recommender')
+#st.image("./gojo.png")
 
 # Query text
 query_text = st.text_input('Ask for an anime recommendation', placeholder = "What's an anime with lots of action?")
@@ -11,5 +12,5 @@ query_text = st.text_input('Ask for an anime recommendation', placeholder = "Wha
 with st.form('myform', clear_on_submit=True):
   response = generate_response(query_text)
 
-st.info(response["answer"])
-st.info(response["sources"])
+st.info(f"{response['answer']}\nMore info: {response['sources']}")
+#st.text(f"More info: {response['sources']}")
